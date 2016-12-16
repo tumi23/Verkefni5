1d2c7c305cd2d19c27e762a0b460330e2b4a4c15
tryggvi13 <tryggvi13@ru.is>
2016-12-16 17:29:06 +0000

    Added menu

--- test.py
+++ test.py
@@ -295,231 +295,296 @@ class BrickWall(pygame.sprite.Sprite):
 
 # Call this function so the Pygame library can initialize itself
 pygame.init()
+while (True):
+    class GameMenu():
+        def __init__(self, screen, items, bg_color=(0,0,0), font=None, font_size=30,
+                        font_color=(255, 255, 255)):
+            self.screen = screen
+            self.scr_width = self.screen.get_rect().width
+            self.scr_height = self.screen.get_rect().height
+     
+            self.bg_color = bg_color
+            self.clock = pygame.time.Clock()
+     
+            self.items = items
+            self.font = pygame.font.SysFont(font, font_size)
+            self.font_color = font_color
+     
+            self.items = []
+            for index, item in enumerate(items):
+                label = self.font.render(item, 1, font_color)
+     
+                width = label.get_rect().width
+                height = label.get_rect().height
+     
+                posx = (self.scr_width / 2) - (width / 2)
+                # t_h: total height of text block
+                t_h = len(items) * height
+                posy = (self.scr_height / 2) - (t_h / 2) + (index * height)
+     
+                self.items.append([item, label, (width, height), (posx, posy)])
+     
+        def run(self):
+            mainloop = True
+            while mainloop:
+                # Limit frame speed to 50 FPS
+                self.clock.tick(50)
+     
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        mainloop = False
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                            mainloop = False
+                        if event.key == pygame.K_ESCAPE:
+                            pygame.quit()
+                            
+                            break
+                # Redraw the background
+                self.screen.fill(self.bg_color)
+                screen.blit(background, (0, 0))
+     
+                for name, label, (width, height), (posx, posy) in self.items:
+                    self.screen.blit(label, (posx, posy))
+     
+                pygame.display.flip()
+     
+     
+    if __name__ == "__main__":
+        # Creating the screen
+        screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT], 0, 32)
+     
+        menu_items = ('Press Space to start', 'Press Esc to Quit')
+     
+        pygame.display.set_caption('Game Menu')
+        gm = GameMenu(screen, menu_items)
+        gm.run()
 
-# Create an 800x600 sized screen
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    # Create an 800x600 sized screen
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
-# Create Background
-screen.blit(background,(0,0))
-bg = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-# Set the title of the window
-pygame.display.set_caption('Test')
-
-# List to hold all the sprites
-all_sprite_list = pygame.sprite.Group()
-
-# Make the walls. (x_pos, y_pos, width, height)
-wall_list = pygame.sprite.Group()
-
-#Make the brick walls
-brickWall_list = pygame.sprite.Group()
-
-#List of all bombs
-bombs_list = pygame.sprite.Group()
-
-#List of blasts
-blast_list = pygame.sprite.Group()
+    # Create Background
+    screen.blit(background,(0,0))
+    bg = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
-#Left
-tY = 0
-while tY < 780:
-    wall = Wall(0, tY, 60, 60)
-    wall_list.add(wall)
-    all_sprite_list.add(wall)
-    tY += 60
+    # Set the title of the window
+    pygame.display.set_caption('Test')
 
-#Top
-tX = 60
-while tX < 900:
-    wall = Wall(tX, 0, 60, 60)
-    wall_list.add(wall)
-    all_sprite_list.add(wall)
-    tX += 60
+    # List to hold all the sprites
+    all_sprite_list = pygame.sprite.Group()
 
-#Right
-tY = 0
-while tY < 780:
-    wall = Wall(SCREEN_WIDTH-60, tY, 60, 60)
-    wall_list.add(wall)
-    all_sprite_list.add(wall)
-    tY += 60
+    # Make the walls. (x_pos, y_pos, width, height)
+    wall_list = pygame.sprite.Group()
 
-#Bottom
-tX = 60
-while tX < 900:
-    wall = Wall(tX, SCREEN_HEIGHT-60, 60, 60)
-    wall_list.add(wall)
-    all_sprite_list.add(wall)
-    tX += 60
+    #Make the brick walls
+    brickWall_list = pygame.sprite.Group()
 
-#Create all walls in the middle of the board
-start = 120
-for i in range(6):
-    gap = 0
-    for j in range(5):
-        wall = Wall(start, 120+gap, 60, 60)
+    #List of all bombs
+    bombs_list = pygame.sprite.Group()
+
+    #List of blasts
+    blast_list = pygame.sprite.Group()
+
+
+    #Left
+    tY = 0
+    while tY < 780:
+        wall = Wall(0, tY, 60, 60)
         wall_list.add(wall)
         all_sprite_list.add(wall)
-        gap += 120
-    start += 120
+        tY += 60
 
-# 2 - is a wall, 1 - are zones not available for brickwall, 0 - are zones available, 3 = brickwall
-level = [
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2],
-    [2, 1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 1, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 1, 2],
-    [2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], ]
+    #Top
+    tX = 60
+    while tX < 900:
+        wall = Wall(tX, 0, 60, 60)
+        wall_list.add(wall)
+        all_sprite_list.add(wall)
+        tX += 60
 
-x, y = 0, 0
-coordinates = []
-for i in range(13):
-    sub = []
-    x = 0
-    for j in range(15):
-        sub.append((x,y))
-        x += 60
-    coordinates.append(sub)
-    y += 60
+    #Right
+    tY = 0
+    while tY < 780:
+        wall = Wall(SCREEN_WIDTH-60, tY, 60, 60)
+        wall_list.add(wall)
+        all_sprite_list.add(wall)
+        tY += 60
 
-for i in coordinates:
-    print(i)
+    #Bottom
+    tX = 60
+    while tX < 900:
+        wall = Wall(tX, SCREEN_HEIGHT-60, 60, 60)
+        wall_list.add(wall)
+        all_sprite_list.add(wall)
+        tX += 60
 
-#Creating random brick walls in empty slots
-numberOfBrickWalls = 0
-while numberOfBrickWalls < MAX_NO_OF_BRICKWALLS:
-    x1 = randint(0, 14)
-    y1 = randint(0, 12)
-    if level[y1][x1] == 0:
-        level[y1][x1] = 3
-        brickWall = BrickWall(60*x1, 60*y1, 60, 60)
-        brickWall_list.add(brickWall)
-        wall_list.add(brickWall)
-        all_sprite_list.add(brickWall)
-        numberOfBrickWalls += 1
+    #Create all walls in the middle of the board
+    start = 120
+    for i in range(6):
+        gap = 0
+        for j in range(5):
+            wall = Wall(start, 120+gap, 60, 60)
+            wall_list.add(wall)
+            all_sprite_list.add(wall)
+            gap += 120
+        start += 120
 
+    # 2 - is a wall, 1 - are zones not available for brickwall, 0 - are zones available, 3 = brickwall
+    level = [
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2],
+        [2, 1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 1, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [2, 1, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 1, 2],
+        [2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], ]
 
-# Create the player paddle object
-player1 = Player(780, 660)
-player2 = Player(60,60)
-player1.walls = wall_list
-player2.walls = wall_list
-all_sprite_list.add(player1)
-all_sprite_list.add(player2)
+    x, y = 0, 0
+    coordinates = []
+    for i in range(13):
+        sub = []
+        x = 0
+        for j in range(15):
+            sub.append((x,y))
+            x += 60
+        coordinates.append(sub)
+        y += 60
 
-clock = pygame.time.Clock()
+    for i in coordinates:
+        print(i)
 
-carryOnMyWaywardSon = True
-
-
-
-while carryOnMyWaywardSon:
-
-    for b in bombs_list:
-        if b.time + 3000 < pygame.time.get_ticks():
-            blast = b.detonate()
-            bombs_list.remove(b)
-            all_sprite_list.remove(b)
-            blast_list.add(blast)
-            all_sprite_list.add(blast)
-
-
-    for bl in blast_list:
-        if bl.time + 500 < pygame.time.get_ticks():
-            blast_list.remove(bl)
-            all_sprite_list.remove(bl)
-
-    blast_collision_list = pygame.sprite.spritecollide(player1, blast_list, False)
-    for p in blast_collision_list:
-        print(p)
-        print("Yo player 1, yu Dead!")
-        # End Of Game
-        carryOnMyWaywardSon = False
-        break
-    blast_collision_list = pygame.sprite.spritecollide(player2, blast_list, False)
-    for p in blast_collision_list:
-        print(p)
-        print("Yo player 2, yu  Dead!")
-        # End Of Game
-        carryOnMyWaywardSon = False
-        break
+    #Creating random brick walls in empty slots
+    numberOfBrickWalls = 0
+    while numberOfBrickWalls < MAX_NO_OF_BRICKWALLS:
+        x1 = randint(0, 14)
+        y1 = randint(0, 12)
+        if level[y1][x1] == 0:
+            level[y1][x1] = 3
+            brickWall = BrickWall(60*x1, 60*y1, 60, 60)
+            brickWall_list.add(brickWall)
+            wall_list.add(brickWall)
+            all_sprite_list.add(brickWall)
+            numberOfBrickWalls += 1
 
 
+    # Create the player paddle object
+    player1 = Player(780, 660)
+    player2 = Player(60,60)
+    player1.walls = wall_list
+    player2.walls = wall_list
+    all_sprite_list.add(player1)
+    all_sprite_list.add(player2)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+    clock = pygame.time.Clock()
 
-        elif event.type == pygame.KEYDOWN:
-            #TODO functionize
-            if event.key == pygame.K_LEFT:
-                player1.changespeed(-3, 0)
-            elif event.key == pygame.K_RIGHT:
-                player1.changespeed(3, 0)
-            elif event.key == pygame.K_UP:
-                player1.changespeed(0, -3)
-            elif event.key == pygame.K_DOWN:
-                player1.changespeed(0, 3)
-            elif event.key == pygame.K_SPACE:
-                bomb = Bomb(player1)
-                all_sprite_list.add(bomb)
-                bombs_list.add(bomb)
-                last_bomb_time = pygame.time.get_ticks()
-                
-            elif event.key == pygame.K_a:
-                player2.changespeed(-3, 0)
-            elif event.key == pygame.K_d:
-                player2.changespeed(3, 0)
-            elif event.key == pygame.K_w:
-                player2.changespeed(0, -3)
-            elif event.key == pygame.K_s:
-                player2.changespeed(0, 3)
-            elif event.key == pygame.K_q:
-                bomb = Bomb(player2)
-                all_sprite_list.add(bomb)
-                bombs_list.add(bomb)
-                last_bomb_time = pygame.time.get_ticks()
-                
-            elif event.key == pygame.K_ESCAPE:
-                carryOnMyWaywardSon = False
+    carryOnMyWaywardSon = True
 
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                player1.changespeed(3, 0)
-            elif event.key == pygame.K_RIGHT:
-                player1.changespeed(-3, 0)
-            elif event.key == pygame.K_UP:
-                player1.changespeed(0, 3)
-            elif event.key == pygame.K_DOWN:
-                player1.changespeed(0, -3)
-                
-            elif event.key == pygame.K_a:
-                player2.changespeed(3, 0)
-            elif event.key == pygame.K_d:
-                player2.changespeed(-3, 0)
-            elif event.key == pygame.K_w:
-                player2.changespeed(0, 3)
-            elif event.key == pygame.K_s:
-                player2.changespeed(0, -3)
 
-    screen.blit(background, (0, 0))
-    all_sprite_list.update()
 
-    all_sprite_list.draw(screen)
+    while carryOnMyWaywardSon:
 
-    pygame.display.flip()
+        for b in bombs_list:
+            if b.time + 3000 < pygame.time.get_ticks():
+                blast = b.detonate()
+                bombs_list.remove(b)
+                all_sprite_list.remove(b)
+                blast_list.add(blast)
+                all_sprite_list.add(blast)
 
-    clock.tick(60)
+
+        for bl in blast_list:
+            if bl.time + 500 < pygame.time.get_ticks():
+                blast_list.remove(bl)
+                all_sprite_list.remove(bl)
+
+        blast_collision_list = pygame.sprite.spritecollide(player1, blast_list, False)
+        for p in blast_collision_list:
+            print(p)
+            print("Yo player 1, yu Dead!")
+            # End Of Game
+            carryOnMyWaywardSon = False
+            break
+        blast_collision_list = pygame.sprite.spritecollide(player2, blast_list, False)
+        for p in blast_collision_list:
+            print(p)
+            print("Yo player 2, yu  Dead!")
+            # End Of Game
+            carryOnMyWaywardSon = False
+            mainloop = True
+            break
+
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            elif event.type == pygame.KEYDOWN:
+                #TODO functionize
+                if event.key == pygame.K_LEFT:
+                    player1.changespeed(-3, 0)
+                elif event.key == pygame.K_RIGHT:
+                    player1.changespeed(3, 0)
+                elif event.key == pygame.K_UP:
+                    player1.changespeed(0, -3)
+                elif event.key == pygame.K_DOWN:
+                    player1.changespeed(0, 3)
+                elif event.key == pygame.K_SPACE:
+                    bomb = Bomb(player1)
+                    all_sprite_list.add(bomb)
+                    bombs_list.add(bomb)
+                    last_bomb_time = pygame.time.get_ticks()
+                    
+                elif event.key == pygame.K_a:
+                    player2.changespeed(-3, 0)
+                elif event.key == pygame.K_d:
+                    player2.changespeed(3, 0)
+                elif event.key == pygame.K_w:
+                    player2.changespeed(0, -3)
+                elif event.key == pygame.K_s:
+                    player2.changespeed(0, 3)
+                elif event.key == pygame.K_q:
+                    bomb = Bomb(player2)
+                    all_sprite_list.add(bomb)
+                    bombs_list.add(bomb)
+                    last_bomb_time = pygame.time.get_ticks()
+                    
+                elif event.key == pygame.K_ESCAPE:
+                    carryOnMyWaywardSon = False
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player1.changespeed(3, 0)
+                elif event.key == pygame.K_RIGHT:
+                    player1.changespeed(-3, 0)
+                elif event.key == pygame.K_UP:
+                    player1.changespeed(0, 3)
+                elif event.key == pygame.K_DOWN:
+                    player1.changespeed(0, -3)
+                    
+                elif event.key == pygame.K_a:
+                    player2.changespeed(3, 0)
+                elif event.key == pygame.K_d:
+                    player2.changespeed(-3, 0)
+                elif event.key == pygame.K_w:
+                    player2.changespeed(0, 3)
+                elif event.key == pygame.K_s:
+                    player2.changespeed(0, -3)
+
+        screen.blit(background, (0, 0))
+        all_sprite_list.update()
+
+        all_sprite_list.draw(screen)
+
+        pygame.display.flip()
+
+        clock.tick(60)
 
 pygame.quit()
